64df09a0dc56be7de1d7dafb5d83d5b4dd96d061
IpsalKnk <ipsal.skunak24@students.utdi.ac.id>
2025-09-06 23:59:52 +0700

    Add files via upload

---
+++ in1.js
@@ -0,0 +1,34 @@
+// Toggle menu mobile
+const menuToggle = document.getElementById('menuToggle');
+const navLinks = document.getElementById('navLinks');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
+// Smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+    navLinks.classList.remove('active'); // tutup menu
+  });
+});
+
+// Scroll reveal animasi
+function revealOnScroll() {
+  const reveals = document.querySelectorAll(".reveal");
+  for (let i = 0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals[i].getBoundingClientRect().top;
+    const revealPoint = 100;
+
+    if (elementTop < windowHeight - revealPoint) {
+      reveals[i].classList.add("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", revealOnScroll);
